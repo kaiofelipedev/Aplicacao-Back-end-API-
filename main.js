0b905eef2989dab7947839f3fe52bcf0e1da2259
kaiofelipedev <kaio.felipe.santos@hotmail.com>
2025-05-31 00:32:21 -0300

    Estruturação da arquitetuta MVC.31/05/2025

--- main.js
+++ main.js
@@ -5,3 +5,14 @@
  *  Versão: 1.0
  **************************************************************/
 
+// Import dos módulos 'express' e 'cors'
+const Express = require('express')
+const cors    = require('cors')
+
+// Instância do módulo 'Express'
+const app = Express()
+
+// Teste de conexão
+app.listen(8080, () => {
+    console.log('Servidor rodando na porta 8080...')
+})
