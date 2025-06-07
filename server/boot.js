036dcee70849d6c2cc0c8a44fd4a469b4610a327
kaiofelipedev <kaio.felipe.santos@hotmail.com>
2025-06-07 00:20:37 -0300

    Atualização

--- server/boot.js
+++ server/boot.js
@@ -5,9 +5,19 @@
  *  Versão: 1.0
  **************************************************************/
 
+<<<<<<< HEAD
 module.exports = (app) => {
+=======
+const Start = function(app){
+>>>>>>> 40dd87e (Criação do boot.js.31/05/2025)
     app.set('port', 8080)
     app.listen(app.get('port'), () => {
         console.log(`Servidor rodando em http://localhost:8080 ...`)
     })
+<<<<<<< HEAD
 }
+=======
+}
+
+module.exports = { Start }
+>>>>>>> 40dd87e (Criação do boot.js.31/05/2025)
